a8b729f3befab4f05036531c29bf794e229a4ddb
Foysal Ahmed <foysalxahmed@gmail.com>
2023-04-16 14:14:01 +0600

    Update Navbar

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,6 +1,7 @@
 import PrimaryButton from '@/components/common/PrimaryButton'
 import HamburgerMenu from '@/components/Layout/HamburgerMenu'
 import { memo, useState } from 'react'
+import classNames from 'classnames'
 
 const Navbar = () => {
   const [open, setOpen] = useState<boolean>(false)
@@ -16,19 +17,14 @@ const Navbar = () => {
 
   return (
     <div className='fixed top-0 z-20 min-h-0 w-full bg-neutral-900 backdrop-blur-md'>
-      <div className='container h-full max-w-screen-xl'>
-        <nav className='relative flex h-full py-2 text-primary lg:items-center lg:justify-between lg:py-6'>
+      <div className='container relative h-full max-w-screen-xl'>
+        <nav className='flex h-full items-center border-red-400 py-4 text-primary lg:justify-between lg:py-6'>
           {/* If there is any space in font name, add underscore instead of space */}
-          <span className='heading-4 flex h-[45px]  items-center  lg:h-auto'>
+          <span className='heading-4 flex items-center lg:h-auto'>
             Mosharrof
             <span className='font-extrabold text-primary-500'>.</span>
           </span>
-          <ul
-            className={
-              open
-                ? 'mt-10 flex h-[330px] grow flex-col items-center gap-4 overflow-hidden transition-[height] duration-300 ease-in-out  lg:mt-0 lg:grow-0 lg:flex-row lg:gap-12 lg:overflow-visible'
-                : 'mt-10 flex h-0 grow flex-col items-center gap-4 overflow-hidden   transition-[height] duration-300 ease-in-out  lg:mt-0 lg:grow-0 lg:flex-row lg:gap-10 lg:overflow-visible'
-            }>
+          <ul className=' mt-0 hidden h-0 flex-row  items-center gap-10 transition-[height] duration-300 ease-in-out lg:flex lg:grow-0'>
             <li>
               <a href='#'>
                 <p className='body-1 text-accent dark:text-accent'>Home</p>
@@ -64,19 +60,58 @@ const Navbar = () => {
                 <p className='body-1'>Contact</p>
               </a>
             </li>
-            <li className='inline-block lg:hidden'>
-              <PrimaryButton size='sm' onClick={onHireMeButtonClick}>
-                Hire Me
-              </PrimaryButton>
-            </li>
           </ul>
-          <div className='hidden lg:inline-block'>
-            <PrimaryButton size='sm' onClick={onHireMeButtonClick}>
-              Hire Me
+          <div className='ml-auto inline-block lg:ml-0'>
+            <PrimaryButton size='xs' onClick={onHireMeButtonClick}>
+              Hire Me!
             </PrimaryButton>
           </div>
-          <div className='flex lg:hidden lg:items-center'>
-            <HamburgerMenu open={open} onClick={onMenuClick} />
+          <div className='ml-4 flex bg-neutral-800 lg:hidden lg:items-center'>
+            <HamburgerMenu isOpen={open} onClick={onMenuClick} />
+          </div>
+
+          <div
+            className={classNames(
+              'absolute top-full -ml-4 h-0 w-full overflow-hidden bg-neutral-800 backdrop-blur-md transition-[height] duration-300 ease-in-out md:-ml-8 lg:hidden',
+              { 'h-[296px]': open }
+            )}>
+            <ul className='flex flex-col items-center gap-4 py-4'>
+              <li>
+                <a href='#'>
+                  <p className='body-1 text-accent dark:text-accent'>Home</p>
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <p className='body-1'>About me</p>
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <p className='body-1'>Service</p>
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <p className='body-1'>Resume</p>
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <p className='body-1'>Portfolio</p>
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <p className='body-1'>Blog</p>
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <p className='body-1'>Contact</p>
+                </a>
+              </li>
+            </ul>
           </div>
         </nav>
       </div>
